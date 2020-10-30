472fe8608bd731910cf52c2264e66e6e35e04865
N <nathan724@gmail.com>
2020-10-30 19:10:47 +0800

    Renamed from mod blocks

--- src/main/java/longerste/warmod/TBaseBlocks.java
+++ src/main/java/longerste/warmod/TBaseBlocks.java
@@ -2,10 +2,17 @@ package longerste.warmod;
 
 import longerste.warmod.blocks.Foundation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ModBlocks {
+public class TBaseBlocks {
 
   @GameRegistry.ObjectHolder("warmod:foundation")
   public static Foundation foundation;
+
+  @SideOnly(Side.CLIENT)
+  public static void initModels() {
+    foundation.initModel();
+  }
 
 }
